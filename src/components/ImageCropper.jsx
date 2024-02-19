7abb55abd1f1f00cc837d6030bf0d82d89e4f065
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-19 12:43:51 +0530

    added the crop festure and the size error of the image

--- src/components/ImageCropper.jsx
+++ src/components/ImageCropper.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
+import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
+
+const MIN_DIMENSION = 150;
+const ASPECT_RATIO = 1;
 
 const ImageCropper = () => {
   const [imgSrc, setImgSrc] = useState("");
+  const [crop, setCrop] = useState();
+  const [error, setError] = useState("");
   const onSelectFile = (e) => {
     const file = e.target.files?.[0];
 
@@ -13,10 +19,33 @@ const ImageCropper = () => {
     reader.addEventListener("load", () => {
       console.log("inside lister");
       const imageUrl = reader.result?.toString() || "";
-      console.log("image data url", imageUrl);
+      // console.log("image data url", imageUrl);
       setImgSrc(imageUrl);
     });
     reader.readAsDataURL(file);
+  };
+
+  const onImageLoad = (e) => {
+    const { width, height, naturalWidth, naturalHeight } = e.currentTarget;
+
+    if (naturalWidth < MIN_DIMENSION || naturalHeight < MIN_DIMENSION) {
+      setError("Image must be at least 150 X 150 pixels.");
+      setImgSrc("");
+      return;
+    }
+    const crop = makeAspectCrop(
+      {
+        unit: "%",
+        width: 25,
+      },
+      ASPECT_RATIO,
+      width,
+      height
+    );
+
+    const centeredCrop = centerCrop(crop, width, height);
+
+    setCrop(centeredCrop);
   };
 
   return (
@@ -30,7 +59,26 @@ const ImageCropper = () => {
           className="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-2 file:rounded-full file:border-0 file:text-xs file:bg-gray-700 file:text-sky-300 hover:file:bg-gray-600"
         />
       </label>
-      {imgSrc && <div className="flex flex-col items-center"></div>}
+      {error && <p className="text-red-400 text-xs">{error}</p>}
+      {imgSrc && (
+        <div className="flex flex-col items-center">
+          <ReactCrop
+            crop={crop}
+            onChange={(pixelCrop, percentCrop) => setCrop(percentCrop)}
+            circularCrop
+            aspect={ASPECT_RATIO}
+            keepSelection
+            minWidth={MIN_DIMENSION}
+          >
+            <img
+              src={imgSrc}
+              alt="Upload"
+              style={{ maxHeight: "70vh" }}
+              onLoad={onImageLoad}
+            />
+          </ReactCrop>
+        </div>
+      )}
     </>
   );
 };
